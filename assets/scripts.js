798af0b57dc7dd535f2eb8c784db5b0914fcb7bd
Usman Rajput <usmanrana18989@gmail.com>
2022-11-24 13:23:58 +0500

    Child Theme First Form Update

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,32 +1,24 @@
 jQuery(document).ready(function($) {
 	// console.log('UR here!');
-    
-    
 
-    $('.r_o_i_box').click(function () {
-        // console.log("Box Clicked!");
+    $('.roi_box_js').on('click',function () {
 
-        $('.r_o_i_box .r_o_i').removeAttr('checked');
-        $(this).find('[name=r_o_i]').attr("checked", "checked");
+        $('input[name=r_o_i]').removeAttr('checked');
+        $(this).find('input[name=r_o_i]').prop("checked", true);
 
-
-        $('.r_o_i_box').removeClass('active');
+        $('.roi_box_js').removeClass('active');
         $(this).addClass('active');
 
-
-        let val = $('input[name="r_o_i"]:checked').val();
-
-        // console.log(val);
-
+        let val = $("input[name=r_o_i]:checked").val();
         ur_adding_disable(val);
+        change_values(val);
+    });
     
-    })
-
-    
-
     function ur_adding_disable(val){
-        // console.log("function called!", val);
-        $('.r_o_i_box').removeClass('ur_disabled');
+        
+        $('.ioi_box').removeClass('active');
+        $('.ioi_box').removeClass('ur_disabled');
+        
         if (val == 4) {
             $('.ioi_box_3, .ioi_box_4, .ioi_box_5').addClass('ur_disabled');
         }
@@ -39,8 +31,55 @@ jQuery(document).ready(function($) {
         if (val == 10) {
             $('.ioi_box_1').addClass('ur_disabled');
         }
-        $(".r_o_i_box.ur_disabled").unbind();
+
+        
     }
 
+    $('.ioi_box').on('click', function () {
+        // $(".ioi_box.ur_disabled").unbind();
+        // console.log("ioi_box box clicked");
 
+        $('.ioi_box .i_o_i').removeAttr('checked');
+        $(this).find('[name=i_o_i]').prop("checked", true);
+
+        $('.ioi_box').removeClass('active');
+        $(this).addClass('active');
+
+    });
+
+
+    $('.ur_pb_submit').click(function (e) {
+        e.preventDefault();
+        // console.log("PB form submitted");
+        
+
+        let val = $("input[name=r_o_i]:checked").val();
+        
+        change_values(val);
+        
+        $('.ur_result').show();
+
+    })
+
+    function change_values(val) {
+        if (val == 4) {
+            head = "Conservative";
+            para = "You are a conservative investor. You seek to preserve your wealth while earning modest returns. You do not like the idea of a market downturn and will prepare ahead of time to minimize losses during such events. You wish to achieve consistent returns even if it means giving up some upside."
+        }
+        if (val == 6) {
+            head = "Moderately Conservative";
+            para = "You are a balanced investor who leans towards slightly below average risk. You seek good returns while ensuring a minimum level of consistent returns. You do not panic in a market downturn, but you are cautious. You try to balance between risk and reward without either excessive risk or minimal returns.";
+        }
+        if (val == 8) {
+            head = "Moderately Aggressive";
+            para = "You are a balanced investor who leans towards slightly above average risk. You seek above average returns while limiting potential downside. You do not panic in a market downturn. You try to balance between risk and reward, but prefer higher returns even if it means slightly higher risk";
+        }
+        if (val == 10) {
+            head = "Aggressive";
+            para = "You are an aggressive investor. You have exceptional tolerance for market fluctuations. You are willing to accept losses for a chance to earn higher returns. You seek higher returns even if it means sacrificing consistent returns in the short term. You muster your emotions in a market downturn and stick to your investment plan.";
+        }
+
+        $('#ur_result_head').html(head);
+        $('#ur_result_para').html(para);
+    }
 });
--- forms/portfolio_builder.php
+++ forms/portfolio_builder.php
@@ -19,23 +19,24 @@ function ur_portfolio_builder_fields(){
                 <label class="ur_label">Return You Expect on your investment?</label>
 
                 <div class="ur_chkbox_group">
-                    <div class="r_o_i_box">
+                    <div class="r_o_i_box roi_box_js">
                         <label for="">4%</label>
-                        <input type="radio" value="4" name="r_o_i" hidden>
+                        <input type="radio" value="4" class="roi_radio" name="r_o_i" hidden >
                     </div>
-                    <div class="r_o_i_box">
+                    <div class="r_o_i_box roi_box_js">
                         <label for="">6%</label>
-                        <input type="radio" value="6" name="r_o_i" hidden>
+                        <input type="radio" value="6" class="roi_radio" name="r_o_i" hidden>
                     </div>
-                    <div class="r_o_i_box">
+                    <div class="r_o_i_box roi_box_js">
                         <label for="">8%</label>
-                        <input type="radio" value="8" name="r_o_i" hidden>
+                        <input type="radio" value="8" class="roi_radio" name="r_o_i" hidden>
                     </div>
-                    <div class="r_o_i_box">
+                    <div class="r_o_i_box roi_box_js">
                         <label for="">10%</label>
-                        <input type="radio" value="10" name="r_o_i" hidden>
+                        <input type="radio" value="10" class="roi_radio" name="r_o_i" hidden>
                     </div>
                 </div>
+      
             </div>
 
             <div class="field">
@@ -44,27 +45,27 @@ function ur_portfolio_builder_fields(){
 
                 <div class="ur_chkbox_group">
                     
-                    <div class="r_o_i_box ioi_box_1" >
+                    <div class="r_o_i_box ioi_box ioi_box_1" >
                         <label for="">0%</label>
                         <input type="radio" value="0" name="i_o_i" hidden>
                     </div>
                     
-                    <div class="r_o_i_box ioi_box_2">
+                    <div class="r_o_i_box ioi_box ioi_box_2">
                         <label for="">2%</label>
                         <input type="radio" value="2" name="i_o_i" hidden>
                     </div>
 
-                    <div class="r_o_i_box ioi_box_3">
+                    <div class="r_o_i_box ioi_box ioi_box_3">
                         <label for="">3%</label>
                         <input type="radio" value="3" name="i_o_i" hidden>
                     </div>
 
-                    <div class="r_o_i_box ioi_box_4">
+                    <div class="r_o_i_box ioi_box ioi_box_4">
                         <label for="">4%</label>
                         <input type="radio" value="4" name="i_o_i" hidden>
                     </div>
 
-                    <div class="r_o_i_box ioi_box_5">
+                    <div class="r_o_i_box ioi_box ioi_box_5">
                         <label for="">5%</label>
                         <input type="radio" value="5" name="i_o_i" hidden>
                     </div>
@@ -74,16 +75,26 @@ function ur_portfolio_builder_fields(){
 
             <div class="ur_btn">
                 <input type="submit" value="Check My Result" class="ur_pb_submit">
+                <!-- <button class="ur_pb_submit">Check My Result</button> -->
             </div>
         </form>
-            
-    </div>
 
-    <div class="ur_overlay">
-        <form action="#">
+        <div class="ur_result">
+            <p class="tophead">Investment Style</p>
+            <h3 id="ur_result_head">Conservative</h3>
+            <p id="ur_result_para">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem alias voluptatibus deserunt eveniet architecto sunt doloribus, nisi quia laborum facilis quo assumenda eius vel veniam voluptate odit molestias dolores tempora cupiditate. Similique nesciunt quidem delectus fugit, consectetur, cumque labore ea aut nisi quod deleniti aperiam. Ab officia eligendi optio tempore.</p>
+        </div>
             
-        </form>
     </div>
+<!-- 
+    <div class="ur_overlay_bg">
+        <div class="ur_overlay">
+            <h3>Potential Portfolio</h3>
+            <form action="#">
+
+            </form>
+        </div>
+    </div> -->
 
     <?php  
 
